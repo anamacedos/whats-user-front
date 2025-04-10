60997e8fc1dd1ea7028c96cff538991331e41cea
ANA JÚLIA SILVA MACEDO <24122496@CFP127ALUNO.LOCAL>
2025-04-10 10:11:08 -0300

    end point de conversas finalizado

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ async function criarCardContato(nomeContato){
     const divDosContatos = document.getElementById('lista_contatos')
     const pContato = document.createElement('p')
     pContato.textContent = nomeContato
+
+
+    pContato.addEventListener('click', function(){
+        trazerConversas(nomeContato)
+    })
+
     divDosContatos.appendChild(pContato)
 
     pContato.classList.add('nomeContato')
@@ -19,25 +25,64 @@ async function trazerContatos(){
 
     return dados
 
-
 }
 
-async function trazerConversas(){
-    const url = `https://giovanna-whatsapp.onrender.com/v1/whatsapp/conversas?numero=11987876567&contato=Jane%20Smith`
+async function trazerConversas(contatoNome){
+    //substitui os espaços por (20%)
+    const nomeFormatado = contatoNome.replaceAll(' ', '%20')
+    const url = `https://giovanna-whatsapp.onrender.com/v1/whatsapp/conversas?numero=11987876567&contato=${nomeFormatado}`
     const response = await fetch(url)
     const data = await response.json()
+
+    //procura a igualdade entre o conversas.nome e o contatoNome(que é o parametro da função,
+    // que é passado a ela quando ela é chamada no onclick, e recebe o mesmo nome que o outro 
+    // endpoint retorna para procurar a igualdade, e armazena tudo issso na variavel conversasDoContato)
+
+    //conversa representa cada item do array data.conversas
+    const conversasDoContato = data.conversas.find(function(conversa){
+        return conversa.name == contatoNome
+    })
+    if (conversasDoContato){
+        exibirMensagens(conversasDoContato.conversas)
+    }else{
+        exibirMensagens([]) //se n achar nada mostra vazio
+    }
+}
+
+async function exibirMensagens(mensagens){
+    const caixaDeMensagens = document.getElementById('caixaDeMensagens')
+    caixaDeMensagens.innerHTML = '' //limpa as mensagens anteriores
+
+    mensagens.forEach(function(mensagem){
+        const divMensagem = document.createElement('div')
+        const conteudoMensagem = document.createElement('p')
+        const horarioMensagem = document.createElement('p')
+
+        conteudoMensagem.textContent = mensagem.content
+        horarioMensagem.textContent = mensagem.time
+
+        if (mensagem.sender == 'me'){
+            divMensagem.classList.add('enviadaPorMim')
+        }else{
+            divMensagem.classList.add('enviadaPeloContato')
+        }
+
+
+
+        divMensagem.appendChild(conteudoMensagem)
+        divMensagem.appendChild(horarioMensagem)
+        caixaDeMensagens.appendChild(divMensagem)
+    })
 }
 
 async function preencherCardContato(){
     const contatos = await trazerContatos()
 
-    const divDosContatos = document.getElementById('lista_contatos')
 
     contatos.forEach(function(contato){
         criarCardContato(contato.name)
     })
 
-    console.log(contatos)
 }
 
 
